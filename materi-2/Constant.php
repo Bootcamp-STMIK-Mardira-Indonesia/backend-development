757a8a5cfbd7b6b82b256dcf9faf7f6829fb1a54
xietsunzao <lordshinjo22@gmail.com>
2023-01-04 10:27:24 +0700

    refactor: :sparkles: Add file materi-2

---
+++ materi-2/Constant.php
@@ -0,0 +1,4 @@
+<?php
+
+const NAME = 'Jefri Maruli';
+echo NAME;
---
+++ materi-2/Variable.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Jefri Maruli H";
+$nim = 16123456;
+$address = "Bandung";
+$email = "jefrimaruli@gmail.com";
+$bootcampClass = "Back-End Developer";
+
+echo "Halo nama saya {$name} <br>
+    nim saya {$nim} <br>
+    alamat saya di {$address} <br>
+    email saya {$email} <br>
+    saya mengikuti bootcamp {$bootcampClass}";
