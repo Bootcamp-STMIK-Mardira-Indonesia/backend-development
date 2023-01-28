b2a4161c0d30972572ddde600f178459d4f6854d
xietsunzao <lordshinjo22@gmail.com>
2023-01-29 01:09:07 +0700

    build(Core): Add method put & header on class Input

--- materi-10/App/Core/Input.php
+++ materi-10/App/Core/Input.php
@@ -19,4 +19,22 @@ class Input
         }
         return $_POST;
     }
+
+    public static function put(string $key = null, $default = null)
+    {
+        parse_str(file_get_contents('php://input'), $put);
+        if ($key) {
+            return $put[$key] ?? $default;
+        }
+        return $put;
+    }
+
+    public static function header(string $key = null, $default = null)
+    {
+        $headers = getallheaders();
+        if ($key) {
+            return $headers[$key] ?? $default;
+        }
+        return $headers;
+    }
 }
