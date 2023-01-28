cf9056d2c2e4cdc43645739d5a7aff48b9676590
xietsunzao <lordshinjo22@gmail.com>
2023-01-28 15:29:48 +0700

    build(Core): :building_construction: Add DotEnvKey for .env

---
+++ materi-10/App/Core/DotenvKey.php
@@ -0,0 +1,13 @@
+<?php
+
+use Dotenv;
+
+class DotEnvKey
+{
+    public static function get(string $key): string
+    {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+        return $_ENV[$key];
+    }
+}
