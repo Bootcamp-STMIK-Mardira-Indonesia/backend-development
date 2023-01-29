6d600907212aaf1330df2657b06c27df9c0ce008
xietsunzao <lordshinjo22@gmail.com>
2023-01-29 12:00:02 +0700

    fix(Core): :bug: Fix bug non statically method

--- materi-10/App/Core/Responses.php
+++ materi-10/App/Core/Responses.php
@@ -4,11 +4,21 @@ namespace App\Core;
 
 trait Responses
 {
-    public function response(int $code, array $data): void
+
+    /**
+     * Response and status code
+     *
+     * @param  mixed $code
+     * @param  mixed $data
+     * @return void
+     */
+    public static function response(int $code, array $data): void
     {
         http_response_code($code);
         header('Content-Type: application/json');
         echo json_encode($data);
         exit;
     }
+
+
 }
